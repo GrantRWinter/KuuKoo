9a3bf677ea5226bcbb0c1c89d87fda2afff27f58
Lauren Hearnden <l.a.hearnden@gmail.com>
2014-01-28 23:35:49 -0800

    have yet to institute sessions

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,18 @@
 module ApplicationHelper
+
+	def resource_name
+    :user
+  end
+
+  def resource
+    @resource ||= User.new
+  end
+
+  def devise_mapping
+    @devise_mapping ||= Devise.mappings[:user]
+  end
+
+
+# http://stackoverflow.com/questions/9381817/devise-sign-up-form-on-the-home-page-as-well
+
 end
