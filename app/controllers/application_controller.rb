2ec5eb87f8bcb584e3409d0e23bdd2768cb2acb9
Grant Winter <grantrwinter@gmail.com>
2014-01-29 23:58:08 +0000

    new users created

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -17,7 +17,7 @@ class ApplicationController < ActionController::Base
   #   @current_user || User.find(session:[:user_id]) if session [:user_id]
   # end
 
-  helper_method :current_user
+ # helper_method :current_user
 
   
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,7 +8,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      session[:user_id] = @user.id
+      #session[:user_id] = @user.id
       redirect_to users_path, notice: "Welcomed and Logged in"
     else
       render :new
@@ -17,13 +17,13 @@ class UsersController < ApplicationController
 
 
   def index
-  	@user = User
+  	@user = User.new
   	@message = Message.all
   end
 
   def show
   	@user = User.find(params[:id])
-    @message = Message.where(user_id: params[:id])
+    #@message = Message.where(user_id: params[:id])
   end
 
   protected
