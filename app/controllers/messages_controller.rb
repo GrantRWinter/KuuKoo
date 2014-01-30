f0091e06a84f94447e6669eae8307399bcf2c128
Lauren Hearnden <l.a.hearnden@gmail.com>
2014-01-30 14:48:30 -0800

    messages save

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -4,24 +4,24 @@ class MessagesController < ApplicationController
   end
 
   def show
-  	#@message = Message.where(id: params[:user_id])
+  	@message = Message.where(id: params[:user_id])
   end
 
   def new
-    @message = Message.new
   	@message = @user.messages.build
   end
 
   def create
-    
-    @message = Message.new(message_params, user_params)
+    @user = User.find(session[:user_id])
+    @message = @user.messages.build(message_params)
     @message.user_id = current_user.id
+    @message = Message.new(message_params)
+   
     if @message.save
-      
-      
-      redirect_to user_messages_path(@user), notice: "Message Saved"
+      session[:user_id] = @user.id
+      redirect_to @user, notice: "Message Saved"
     else
-      redirect_to user_messages_path(@user), notice: "Message Not! Saved"
+      redirect_to @user, notice: "Message Not! Saved"
     end
   
   end
@@ -31,10 +31,6 @@ class MessagesController < ApplicationController
   end
 
   protected
-
-  def user_params
-    params.require(:user).permit(:first_name, :last_name, :id, :email, :phone_number, :password)
-  end
 
   def message_params
     params.require(:message).permit(:user_id, :subject, :recipient, :recipient_email, :recipient_phone_number, :send_time, :text, :created_at, :updated_at)
